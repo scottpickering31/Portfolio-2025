e40f105690f6966fcae0e704052cb8dcd009ae27
Scott Pickering <97522229+scottpickering31@users.noreply.github.com>
2024-01-16 17:15:58 +0000

    feat: Format Technologies.tsx file, add alt text to all <img> elements, add className to all <img> elements, fix incorrect image URL for TailwindCSS logo, fix incorrect image URL for Bootstrap logo and format image URLs  file

--- src/assets/Components/Technologies.tsx
+++ src/assets/Components/Technologies.tsx
@@ -3,52 +3,112 @@ function Technologies() {
     <div className="container text-center">
       <div className="row row-cols-4">
         <div className="col">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+            className="img-fluid"
+            alt="HTML5 Logo"
+          />
         </div>
         <div className="col">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+            className="img-fluid"
+            alt="CSS3 Logo"
+          />
         </div>
         <div className="col">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+            className="img-fluid"
+            alt="Javascript Logo"
+          />
         </div>
         <div className="col">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+            className="img-fluid"
+            alt="React Logo"
+          />
         </div>
         <div className="col">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg"
+            className="img-fluid"
+            alt="Redux Logo"
+          />
         </div>
         <div className="col">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
+            className="img-fluid"
+            alt="NodeJS Logo"
+          />
         </div>
         <div className="col">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"
+            className="img-fluid"
+            alt="Express Logo"
+          />
         </div>
         <div className="col">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"
+            className="img-fluid"
+            alt="MongoDB Logo"
+          />
         </div>
         <div className="col">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg"
+            className="img-fluid"
+            alt="PostgreSQL Logo"
+          />
         </div>
         <div className="col">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" />
+          <img
+            src="images/Postman-Logo.svg"
+            className="img-fluid"
+            alt="Postman Logo"
+          />
         </div>
         <div className="col">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"
+            className="img-fluid"
+            alt="TailwindCSS Logo"
+          />
         </div>
         <div className="col">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
+            className="img-fluid"
+            alt="Bootstrap Logo"
+          />
         </div>
         <div className="col">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg"
+            className="img-fluid"
+            alt="SaSS Logo"
+          />
         </div>
         <div className="col">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain.svg" />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"
+            className="img-fluid"
+            alt="Docker Logo"
+          />
         </div>
         <div className="col">
-          <img src="images/AWS-Logo.svg" />
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain.svg"
+            className="img-fluid"
+            alt="Kubernetes Logo"
+          />
         </div>
         <div className="col">
-          <img src="images/Postman-Logo.svg" />
+          <img src="images/AWS-Logo.svg" className="img-fluid" alt="AWS Logo" />
         </div>
       </div>
     </div>
