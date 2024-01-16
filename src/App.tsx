18a292c705ae387884b324a98feb49c1c16ca18e
Scott Pickering <97522229+scottpickering31@users.noreply.github.com>
2024-01-16 17:08:16 +0000

    feat: Add Technologies component, add ProjectCard component and update Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import Header from "../src/assets/Header/Header";
 import Footer from "../src/assets/Footer/Footer";
+import Technologies from "../src/assets/Components/Technologies";
+import ProjectCard from "./assets/Components/ProjectCard";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Technologies />
+      <ProjectCard />
+      <ProjectCard />
+      <ProjectCard />
       <Footer />
     </div>
   );
---
+++ src/assets/Components/Technologies.tsx
@@ -0,0 +1,76 @@
+function Technologies() {
+  return (
+    <div className="container text-center">
+      <div className="row row-cols-4">
+        <div className="col">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+        </div>
+        <div className="col">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+        </div>
+        <div className="col">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+        </div>
+        <div className="col">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+        </div>
+        <div className="col">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" />
+        </div>
+        <div className="col">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+        </div>
+        <div className="col">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
+        </div>
+        <div className="col">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
+        </div>
+        <div className="col">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
+        </div>
+        <div className="col">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" />
+        </div>
+        <div className="col">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" />
+        </div>
+        <div className="col">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" />
+        </div>
+        <div className="col">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" />
+        </div>
+        <div className="col">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain.svg" />
+        </div>
+        <div className="col">
+          <img src="images/AWS-Logo.svg" />
+        </div>
+        <div className="col">
+          <img src="images/Postman-Logo.svg" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Technologies;
+
+// Tech Images to add:
+// html5
+// css3
+// javascript
+// react
+// redux
+// node.js
+// express
+// mongodb
+// postgresql
+// tailwind
+// bootstrap
+// SaSS
+// Postman
+// AWS
+// Docker
+// Kubernetes
