5bb947c7250dec2d2c094342b1a980d9570725d1
Scott Pickering <97522229+scottpickering31@users.noreply.github.com>
2024-01-18 17:12:56 +0000

    Added Lotties

--- src/assets/Header/Header.tsx
+++ src/assets/Header/Header.tsx
@@ -1,12 +1,47 @@
-function Header({ toggleDarkMode }) {
+import React, { useState } from "react";
+import Lottie from "react-lottie";
+import nightAnimationData from "../../../public/Lotties/NightToDayToggle.json";
+import dayAnimationData from "../../../public/Lotties/DayToNightToggle.json";
+
+function Header({ toggleDarkMode, darkMode }) {
+  const [animationState, setAnimationState] = useState({
+    isStopped: true,
+    isPaused: true,
+  });
+
+  const animationOptions = {
+    loop: false,
+    autoplay: animationState.isStopped,
+    animationData: darkMode ? dayAnimationData : nightAnimationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const handleBackgroundClick = () => {
+    toggleDarkMode();
+    setAnimationState({
+      ...animationState,
+      isStopped: false,
+      isPaused: false,
+    });
+  };
+
   return (
     <div className="container text-center">
       <div className="col">
-        <button className="btn btn-primary mt-5" onClick={toggleDarkMode}>
-          Toggle Dark Mode
-        </button>
+        <div
+          style={{ backgroundColor: darkMode ? "black" : "white" }}
+          onClick={handleBackgroundClick}
+        >
+          <Lottie
+            options={animationOptions}
+            isStopped={animationState.isStopped}
+            isPaused={animationState.isPaused}
+          />
+        </div>
         <div className="col mb-5">
-          <img src="https://picsum.photos/200/300" />
+          <img src="https://picsum.photos/200/300" alt="Profile" />
         </div>
       </div>
     </div>
